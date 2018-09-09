2dfae9e448abb6412d4747bc3d1f884ccf27357c
Joachim Schiele <js@lastlog.de>
2018-09-09 19:47:11 +0200

    added nix-prefetch-git and nix-prefetch-hg as dependency

--- default.nix
+++ default.nix
@@ -6,13 +6,19 @@ buildGoPackage rec {
   goPackagePath = "github.com/nixcloud/dep2nix";
 
   src = ./.;
+  
+  buildInputs = [ makeWrapper ];
+  binPath = lib.makeBinPath [ nix-prefetch-git nix-prefetch-hg ];
 
   goDeps = ./deps.nix;
+
+  postInstall = ''
+    wrapProgram $bin/bin/dep2nix --prefix PATH ':' ${binPath}
+  '';
   
   meta = with stdenv.lib; {
     description = "Convert `Gopkg.lock` files from golang dep into `deps.nix`";
     license = licenses.bsd3;
     homepage = https://github.com/nixcloud.io/dep2nix;
   };
-  
 }
